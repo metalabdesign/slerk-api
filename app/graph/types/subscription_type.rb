388a89d1ad5e9b1eebbdb2243058c3f2ba89ce1d
James Phillips <jamesdphillips@gmail.com>
2015-11-04 09:55:33 -0800

    WIP subscription changes

--- app/graph/types/subscription_type.rb
+++ app/graph/types/subscription_type.rb
@@ -2,9 +2,9 @@ SubscriptionType = GraphQL::ObjectType.define do
   name "Subscription"
   description "The root for subscriptions in this schema"
 
-  field :subscribeToMessages, !MessageType do
-    description("Receive new messages. 😎")
-    argument :channel_id, !types.ID
+  field :newMessage, !MessageType do
+    description("Receive new messages.😎")
+    argument :channelID, !types.ID
     resolve -> (o, args, ctx) {
       nil # TODO ??
     }
